f9ebc9c08b9d69c22144fac92d7ee4b9ec618d62
Bounds <andybounds@gmail.com>
2023-01-23 09:30:17 -0500

    Added assignment 3

---
+++ A3_Month/A3_Month.cpp
@@ -0,0 +1,65 @@
+// A3_Month.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Insert Month Number: ";
+    int month;
+    std::cin >> month;
+
+    switch (month)
+    {
+    case 1:
+        std::cout << "January";
+        break;
+    case 2:
+        std::cout << "Feburary";
+        break;
+    case 3:
+        std::cout << "March";
+        break;
+    case 4:
+        std::cout << "April";
+        break;
+    case 5:
+        std::cout << "May";
+        break;
+    case 6:
+        std::cout << "June";
+        break;
+    case 7:
+        std::cout << "July";
+        break;
+    case 8:
+        std::cout << "August";
+        break;
+    case 9:
+        std::cout << "September";
+        break;
+    case 10:
+        std::cout << "October";
+        break;
+    case 11:
+        std::cout << "November";
+        break;
+    case 12:
+        std::cout << "December";
+        break;
+    default:
+        std::cout << "Invalid Input";
+        break;
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
